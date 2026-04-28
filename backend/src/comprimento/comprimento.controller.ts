1b935f6a983a3428e1eb40bf591911af85935759
Francy L. Schenkel <francylauana11@gmail.com>
2026-04-28 16:43:54 -0300

    .

--- backend/src/comprimento/comprimento.controller.ts
+++ backend/src/comprimento/comprimento.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseFloatPipe } from '@nestjs/common';
 import { ConversorService } from 'src/conversor/conversor.service';
 
 @Controller('comprimento')//decorator de responsabilidade
 export class ComprimentoController {
     constructor(private readonly service:ConversorService){}
+    @Get('metros-para-km/:valor')
+        getKm(@Param('valor',ParseFloatPipe)v:number){
+        return{
+            resultado:this.service.MetrosParaKm(v),
+            unidade:'km'
+        };
+    }
+    @Get('metros-para-milha/:valor')
+        getmilha(@Param('valor',ParseFloatPipe)v:number){
+        return{
+            resultado:this.service.MetrosParaMilhas(v),
+            unidade:'milhas'
+        };
+    }
+    @Get('metros-para-pes/:valor')
+        getpes(@Param('valor',ParseFloatPipe)v:number){
+        return{
+            resultado:this.service.MetrosParaPes(v),
+            unidade:'pés'
+        };
+    }
 }
--- backend/src/conversor/conversor.service.ts
+++ backend/src/conversor/conversor.service.ts
@@ -3,7 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ConversorService {
 //comprimento
-metrosParaKm(valor:number){
+MetrosParaKm(valor:number){
     return valor/1000;
  }
+MetrosParaMilhas(valor:number){
+    return valor/1609;
+}
+MetrosParaPes(valor:number){
+    return valor/3281;
+}
 }
